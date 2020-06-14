9613fcca477ea1bde489b6b9967b7f28c0127558
Pamavoc <58072737+Pamavoc@users.noreply.github.com>
2020-06-14 21:34:52 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <header-top></header-top>
+    <contenu></contenu>
+  </div>
+</template>
+
+<script>
+import Contenu from './components/Contenu.vue'
+import Header from './components/Header/Header.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+   
+   'contenu': Contenu,
+   'header-top': Header
+    
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Contenu.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container mt-5">
+        
+        <h1> Do the job !</h1>
+        
+        <form class="form-group">
+
+            <div>
+                <label for='action'>Les choses doivent se faire</label>
+                <input v-model="formData.tache" type="text" id="action" class="form-control">
+            </div>
+        </form>    
+
+        <button v-on:click.prevent="creationTache" class="btn btn-primary mb-3">Créer une tache</button>
+
+
+        <ul>
+            <li v-bind:key="index" v-for="(tache, index) in tableauTaches">
+                <tache v-bind:id="index" :tache="tache" :suppression="suppression"></tache>
+                <!-- id qui est l'index de l'element du tableau -->
+            </li>
+            
+            
+
+        </ul>
+
+
+    </div>
+    
+
+
+</template>
+
+<script>
+
+//on veut les utiliser partout donc export
+import Tache from './Tache'
+
+
+export default {
+
+
+
+    name:'Contenu',
+    data(){
+        return {
+            formData: {
+                tache: ''
+            },
+            tableauTaches: ['JavaScript', 'Vue', 'Python']
+        }
+    },
+    methods: {
+        creationTache:function() {
+            this.tableauTaches.push(this.formData.tache)
+            this.formData.tache = ''
+            //two ways data-binding
+        },
+
+         suppression:function(e) {
+            //console.log(e.target.parentNode.id);
+            this.tableauTaches.splice(e.target.parentNode.id, 1)
+        },
+     },
+  
+    components: {
+        'tache': Tache
+    }
+    
+}
+
+
+
+</script>
+
+
+
+<style>
+
+
+h1 {
+    margin-top: 100px !important;
+}
+
+ul {
+    list-style-type:none;
+}
+</style>
